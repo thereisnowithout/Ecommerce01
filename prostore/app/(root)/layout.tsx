c5c9082e9ee4a21f549f344bcfd521ffd6b34a28
Brett Grentell <thereisnowithout@gmail.com>
2025-04-02 18:04:10 +1100

    Added root folders and constants

---
+++ prostore/app/(root)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import {Inter} from "next/font/google";
+import '@/assets/styles/globals.css'
+import { APP_NAME } from '@/lib/constants';
+const inter = Inter({ subsets: ['latin']});
+
+export const metadata: Metadata = {
+  title: `${APP_NAME}`,
+  description: "Prostore created with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
